e8016d4345dd00986693306fadb42ff0da4bef99
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-06-22 11:09:26 +0700

    Calculate BMI

--- app/src/main/java/com/rujirakongsomran/kt_bmicalculator/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/kt_bmicalculator/MainActivity.kt
@@ -2,10 +2,19 @@ package com.rujirakongsomran.kt_bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnCalculate.setOnClickListener {
+            val h = (etHeight.text).toString().toFloat() / 100;
+            val w = etWeight.text.toString().toFloat()
+            val bmi = w / (h * h)
+
+            tvResult.text = "%.2f".format(bmi)
+        }
     }
 }
